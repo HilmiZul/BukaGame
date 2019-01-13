e41f39b162b27a0816a0e8764c30155eb994a397
hilmizul <netspytux@gmail.com>
2019-01-13 22:29:45 +0700

    update

--- Boxes.js
+++ Boxes.js
@@ -1,15 +1,15 @@
 class Boxes {
   constructor(stack) {
     this.pos = createVector(width, height - boxImg.height - stack);
-    this.step = 3;
+    this.step = 2;
     this.total = random(0, 5);
   }
 
   show() {
     // let stack = 0;
     // for (let i = 0; i < this.total; i++) {
-      image(boxImg, this.pos.x, this.pos.y);
-      // stack += boxImg.height;
+    image(boxImg, this.pos.x, this.pos.y);
+    // stack += boxImg.height;
     // }
   }
 
